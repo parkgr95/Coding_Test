d27efe4000903cac8cbe12d9dfeb76ceb30f2da6
82106 <pkr951113@gmail.com>
2021-07-26 22:29:16 +0900

    0726(두바퀴)

--- Ch09.최단 경로/9-4.py
+++ Ch09.최단 경로/9-4.py
@@ -1,29 +1,29 @@
+# 0726
+# idea: 노드의 개수가 100개 이하니 플로이드 워셜 알고리즘을 사용하는 게 유리하다. 기존 코드와는 다르게 서로에게 가는 비용이 다 1임을 주의
+# 미래 도시
+import sys
+
 INF = int(1e9)
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
+g = [[INF] * (n+1) for _ in range(n+1)]
 
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
-for a in range(1, n + 1):
-  for b in range(1, n + 1):
-    if a == b:
-      graph[a][b] == 0
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            g[i][j] = 0
 
 for _ in range(m):
-  a, b = map(int, input().split())
-  graph[a][b] = 1
-  graph[b][a] = 1
+    i, j = map(int, sys.stdin.readline().split())
+    g[i][j] = 1
+    g[j][i] = 1
 
-x, k = map(int, input().split())
+x, k = map(int, sys.stdin.readline().split())
 
-for k in range(1, n + 1):
-  for a in range(1, n + 1):
-    for b in range(1, n + 1):
-      graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            g[i][j] = min(g[i][j], g[i][k] + g[k][j])
 
-distance = graph[1][k] + graph[k][x]
-
-if distance >= INF:
-  print("-1")
-else:
-  print(distance)
+ans = g[1][k] + g[k][x]
+print(-1) if ans == INF else print(ans)
--- Ch09.최단 경로/9-5.py
+++ Ch09.최단 경로/9-5.py
@@ -1,37 +1,42 @@
+# 0726
+# idea: n과 m의 범위가 충분히 크기 때문에, 우선순위 큐를 이용해서 다익스트라 알고리즘을 작성하는 것이 유리하다.
+# 전보
 import heapq
 import sys
-input = sys.stdin.readline
+
 INF = int(1e9)
 
-n, m, start = map(int(input().split()))
-graph = [[] for i in range(n + 1)]
-distance = [INF] * (n + 1)
+n, m, c = map(int, sys.stdin.readline().split())
+g = [[] for _ in range(n+1)]
+d = [INF] * (n+1)
 
 for _ in range(m):
-  a, b, c = map(int, input().split())
-  graph[a].append((b, c))
+    x, y, z = map(int, sys.stdin.readline().split())
+    g[x].append((y, z))
 
 def dijkstra(start):
-  q = []
-  heapq.heappush(q, (0, start))
-  distance[start] = 0
-  while q:
-    dist, now = heapq.heappop(q)
-    if distance[now] < dist:
-      continue
-    for i in graph[now]:
-      cost = dist + i[1]
-      if cost < distance[i[0]]:
-        distance[i[0]] = cost
-        heapq.heappush(q, (cost, i[0]))
+    q = []
+    heapq.heappush(q, (0, start))
+    d[start] = 0
+    while q:
+        dist, now = heapq.heappush(q)
+        if d[now] < dist:
+            continue
+        for i in g[now]:
+            cost = dist + i[1]
+            if cost < d[i[0]]:
+                d[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-dijkstra(start)
+dijkstra(c)
+# 도시의 총 개수, 총 걸리는 시간
+cnt, tm = 0, 0
+for i in d:
+    if i != INF:
+        cnt += 1
+        tm += max(max, i)
 
-count = 0
-max_distance = 0
-for d in distance:
-  if d != INF:
-    count += 1
-    max_distance = max(max_distance, d)
+# 시작은 제외
+print(cnt - 1, tm)
 
-print(count - 1, max_distance)
+# 회고: 쓰면서 버벅댔다... 다익스트라 알고리즘을 아직 다 익히지 못한 탓이다... 근 시일내로 다시 한 번 봐야할 듯.
