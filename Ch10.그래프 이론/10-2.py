dcfdfb619b0102bf56a9b492c423a7449974fe7a
82106 <pkr951113@gmail.com>
2021-07-28 23:31:04 +0900

    0728(두바퀴)

--- Ch10.그래프 이론/10-2.py
+++ Ch10.그래프 이론/10-2.py
@@ -1,3 +1,5 @@
+# 0727
+# 경로 압축 기법
 def find_parent(parent, x):
   if parent[x] != x:
     parent[x] = find_parent(parent, parent[x])
--- Ch10.그래프 이론/10-3.py
+++ Ch10.그래프 이론/10-3.py
@@ -1,32 +1,45 @@
+# 0727
+# 개선된 서로소 집합 알고리즘
+import sys
+
+# 특정 원소가 속한 집합을 찾기
 def find_parent(parent, x):
-  if parent[x] != x:
-    parent[x] = find_parent(parent, parent[x])
-  return parent[x]
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
+
+# 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
-  a = find_parent(parent, a)
-  b = find_parent(parent, b)
-  if a < b:
-    parent[b] = a
-  else:
-    parent[a] = b
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
-v, e = map(int, input().split())
-parent = [0] * (v + 1)
 
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int, sys.stdin.readline().split())
+parent = [0] * (v + 1)  # 부모 테이블 초기하
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
 for i in range(1, v + 1):
-  parent[i] = i
+    parent[i] = i
 
+# union 연산을 각각 수행
 for i in range(e):
-  a, b = map(int, input().split())
-  union_parent(parent, a, b)
+    a, b = map(int, sys.stdin.readline().split())
+    union_parent(a, b)
 
-print('각 원소가 속한 집합: ', end = '')
+# 각 원소가 속한 집합 출력
+print('각 원소가 속한 집합: ', end='')
 for i in range(1, v + 1):
-  print(find_parent(parent, i), end = ' ')
-
+    print(find_parent(parent, i), end=' ')
 print()
 
-print('부모 테이블: ', end = '')
+# 부모 테이블 내용 출력
+print('부모 테이블: ', end='')
 for i in range(1, v + 1):
-  print(parent[i], end = ' ')
+    print(parent[i], end=' ')
