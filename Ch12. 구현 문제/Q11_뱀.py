2b90ed5503ae6e2fa5267bd39ba5675344c121c6
82106 <pkr951113@gmail.com>
2021-08-03 23:31:21 +0900

    0803두바퀴)

--- Ch12. 구현 문제/Q11_뱀.py
+++ Ch12. 구현 문제/Q11_뱀.py
@@ -2,25 +2,11 @@
 # 뱀
 # idea: 전형적인 시뮬레이션 문제다. 조건에 따라 코드를 만든다.
 import sys
-
-n = int(sys.stdin.readline().strip())
-arr = [[0] * (n + 1) for _ in range(n + 1)]
-
-k = int(sys.stdin.readline().strip())
-for _ in range(k):
-    a, b = map(int, sys.stdin.readline().split())
-    arr[a][b] = 1
-
-l = int(sys.stdin.readline().strip())
-info = []
-for _ in range(l):
-    x, c = sys.stdin.readline().split()
-    info.append((int(x), c))
+from collections import deque
 
 # 방향: 동, 남, 서, 북
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-
 
 def turn(direction, c):
     if c == 'L':
@@ -29,37 +15,40 @@ def turn(direction, c):
         direction = (direction + 1) % 4
     return direction
 
+def simulate():
+    x, y = 1, 1 # 뱀의 초기 위치
+    arr[x][y] = 2 # 뱀이 있는 곳은 2로 표시
+    direction = 0 # 초기 방향
+    time = 1
+    q = deque([(x, y)]) # 뱀 정보
+    while True:
+        x, y = x + dx[direction], y + dy[direction]
+        if 1 <= x <= n and 1 <= y <= n and arr[x][y] != 2:
+            # 사과가 없으면 꼬리 제거
+            if not arr[x][y] == 1:
+                px, py = q.popleft()
+                arr[px][py] = 0
+            arr[x][y] = 2
+            q.append([x, y])
+            if time in info:
+                direction = turn(direction, info[time])
+            time += 1
+        else:
+            return time
 
-x, y = 1, 1
-arr[x][y] = 2
-direction = 0
-time = 0
-idx = 0
-# 뱀 정보
-q = [(x, y)]
-while True:
-    nx = x + dx[direction]
-    ny = y + dy[direction]
-    if 1 <= nx <= n and 1 <= ny <= n and arr[nx][ny] != 2:
-        # 사과가 없으면
-        if arr[x][y] == 0:
-            arr[nx][ny] = 2
-            q.append((nx, ny))
-            # 꼬리
-            px, py = q.pop(0)
-            arr[px][py] = 0
-        # 사과가 있으면
-        if arr[x][y] == 1:
-            arr[nx][ny] = 2
-            q.append((nx, ny))
-    else:
-        time += 1
-        break
-    x, y = nx, ny
-    time += 1
-    # 방향 변환 정보 시간일 때
-    if idx < l and time == info[idx][0]:
-        direction = turn(direction, info[idx][1])
-        idx += 1
+if __name__ == "__main__":
 
-print(time)
+    n = int(sys.stdin.readline().strip())
+
+    # 맵 정보(사과가 있는 곳은 1)
+    arr = [[0] * (n + 1) for _ in range(n + 1)]
+    for _ in range(int(sys.stdin.readline().strip())):
+        a, b = map(int, sys.stdin.readline().split())
+        arr[a][b] = 1
+
+    # 방향 회전 정보
+    info = {}
+    for _ in range(int(sys.stdin.readline().strip())):
+        x, c = sys.stdin.readline().split()
+        info[int(x)] = c
+    print(simulate())
--- Ch12. 구현 문제/Q12_기둥과 보 설치.py
+++ Ch12. 구현 문제/Q12_기둥과 보 설치.py
@@ -1,11 +1,18 @@
+# 0803
+# 기둥과 보 설치
+# idea: 전형적인 시뮬레이션 문제다. 조건에 따라 코드를 만든다.
 def possible(answer):
     for x, y, stuff in answer:
+        # 기둥일 때
         if stuff == 0:
-            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+            # 바닥 위 혹은 보의 한쪽 끝부분 위 혹은 다른 기둥 위라면 정상
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
                 continue
             return False
+        # 보일 때
         elif stuff == 1:
-            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+            # 한쪽 끝부분이 기둥 위 혹은 양쪽 끝부분이 다른 보와 동시에 연결이라면 정상
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
                 continue
             return False
     return True
@@ -13,13 +20,15 @@ def possible(answer):
 def solution(n, build_frame):
     answer = []
     for frame in build_frame:
-        x, y, stuff, operate = frame
-        if operate == 0:
+        x, y, stuff, op = frame
+        # 삭제하는 경우
+        if op == 0:
             answer.remove([x, y, stuff])
             if not possible(answer):
                 answer.append([x, y, stuff])
-        if operate == 1:
+        # 설치하는 경우
+        if op == 1:
             answer.append([x, y, stuff])
             if not possible(answer):
                 answer.remove([x, y, stuff])
-    return sorted(answer)
+    return sorted(answer, key = lambda x : (x[0], x[1], x[2]))
