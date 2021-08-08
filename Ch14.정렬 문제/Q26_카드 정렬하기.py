5a8802d12fcce312627f0d3925d5d218d0d8885a
82106 <pkr951113@gmail.com>
2021-08-08 23:05:30 +0900

    0808두바퀴)

---
+++ Ch14.정렬 문제/Q26_카드 정렬하기.py
@@ -0,0 +1,20 @@
+# 0808
+# 카드 정렬하기
+# idea: 우선 순위 큐 사용
+import heapq
+import sys
+
+if __name__ == "__main":
+    heap = []
+    for i in range(int(sys.stdin.readline().rstrip())):
+        heapq.heappush(heap, int(sys.stdin.readline().rstrip()))
+
+    ans = 0
+    while len(heap) != 1:
+        f = heapq.heappop(heap) # 가장 작은 값
+        s = heapq.heappop(heap) # 그 다음 작은 값
+        num = f + s
+        ans += num
+        heapq.heappush(heap, num)
+
+    print(ans)
