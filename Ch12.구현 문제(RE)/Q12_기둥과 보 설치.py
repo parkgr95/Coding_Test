f5383118294c704bd764c93f2816086e008dc937
82106 <pkr951113@gmail.com>
2021-12-03 00:29:59 +0900

    1202(세바퀴)

--- Ch12.구현 문제(RE)/Q12_기둥과 보 설치.py
+++ Ch12.구현 문제(RE)/Q12_기둥과 보 설치.py
@@ -23,6 +23,6 @@ def solution(n, build_frame):
             answer.append([x, y, a])
             if not possible(answer):
                 answer.remove([x, y, a])
-    return answer
+    return sorted(answer, key=lambda x: (x[0], x[1], x[2]))
 
 print(solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))
