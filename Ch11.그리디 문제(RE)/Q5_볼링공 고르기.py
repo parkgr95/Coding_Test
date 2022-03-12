353bf1fd750b1577e710f4b5a1459c988cb670c0
parkgr95 <pkr951113@gmail.com>
2022-03-12 22:00:56 +0900

    0312(네바퀴)

--- Ch11.그리디 문제(RE)/Q5_볼링공 고르기.py
+++ Ch11.그리디 문제(RE)/Q5_볼링공 고르기.py
@@ -8,3 +8,19 @@ for i in range(n):
             ans += 1
 
 print(ans)
+
+# 답안지
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+
+array = [0] * 11
+
+for x in data:
+  array[x] += 1
+
+result = 0
+for i in range(1, n + 1):
+  n -= array[i]
+  result += array[i] * n
+
+print(result)
